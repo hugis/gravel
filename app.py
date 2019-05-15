82085ce10b596368dc46c8a6f088d11c8fa410e1
Peter Hyben <peter.hyben@tag.sk>
2019-05-15 09:45:50 +0200

    Display data from last half hour

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import flask
+from sqlalchemy import text
 
 from db import Value, Session
 
@@ -18,29 +19,30 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 
 session = Session()
-data: List[Value] = session.query(Value).order_by(Value.timestamp)[1:1000]
+data: List[Value] = session.query(Value).filter(
+    text("val_dev_id = 1 AND val_timestamp > NOW() - INTERVAL '30 minutes'")
+).order_by(Value.timestamp)
 
 ax = [d.timestamp for d in data]
 
 
-app.layout = html.Div(children=[
-    html.H1(children="Graviton Elektromer"),
-    html.Div(children="Ukážka zobrazenia údajov"),
-
-    dcc.Graph(
-        id="example-graph",
-        figure={
-            "data": [
-                {"x": ax, "y": [d.u_1 for d in data], "name": "Fáza 1"},
-                {"x": ax, "y": [d.u_2 for d in data], "name": "Fáza 2"},
-                {"x": ax, "y": [d.u_3 for d in data], "name": "Fáza 3"},
-            ],
-            "layout": {
-                "title": "Zariadenie 1"
-            }
-        }
-    )
-])
+app.layout = html.Div(
+    children=[
+        html.H1(children="Graviton Elektromer"),
+        html.Div(children="Ukážka zobrazenia údajov"),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {"x": ax, "y": [d.u_1 for d in data], "name": "Fáza 1 - U [V]"},
+                    {"x": ax, "y": [d.u_2 for d in data], "name": "Fáza 2 - U [V]"},
+                    {"x": ax, "y": [d.u_3 for d in data], "name": "Fáza 3 - U [V]"},
+                ],
+                "layout": {"title": "Zariadenie 1"},
+            },
+        ),
+    ]
+)
 
 if __name__ == "__main__":
     app.run_server()
